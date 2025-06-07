8ddc6437590d9775706602274b17f1c1ac249f0b
JACR <98819854+JhojanAlexanderCalambasRamirez@users.noreply.github.com>
2025-06-06 21:20:52 -0500

    Agrega morgan a dependencias

--- functions/services/firebase.js
+++ functions/services/firebase.js
@@ -1,7 +1,7 @@
 // services/firebase.js
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../ruta-valle-service-account.json"); // Asegúrate de tener tu key aquí
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
